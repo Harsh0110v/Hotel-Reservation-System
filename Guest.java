a1c3ea49a3c3dd0862f13461f21705e81376d50d
Harsh Varma <harshvarma0110@gmail.com>
2025-11-29 17:34:24 +0100

    just added age

--- Guest.java
+++ Guest.java
@@ -1,22 +1,27 @@
-public class Guest {
+import java.io.Serializable;
+
+public class Guest implements Serializable{
+    private String memberId;
     private String name;
-    private String email;
     private String phone;
-    private String idNumber;
-    public Guest(String name,String email,String phone,String idNumber){
-        this.name=name;
-        this.email=email;
-        this.phone=phone;
-        this.idNumber=idNumber;
-    }
+    private String email;
+    private int age;
 
-    public String getName(){return name;}
-    public String getEmail(){return email;}
-    public String getPhone(){return phone;}
-    public String getIdNumber(){return idNumber;}
-
-    @Override
-    public String toString() {
-        return String.format("Guest: %s | Email: %s | Phone: %s", name, email, phone);
+    public Guest(String memberId, String name, String phone, String email, int age) {
+        this.memberId = memberId;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.age = age;
     }
+    public String getMemberId() {
+        return memberId; }
+    public String getName() {
+        return name; }
+    public String getPhone() {
+        return phone; }
+    public String getEmail() {
+        return email; }
+    public int getAge() { 
+        return age; }
 }
